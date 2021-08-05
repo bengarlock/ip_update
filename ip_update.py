cdb86fe3b81721de6b2187cfe28f5a86fe938910
bengarlock <ben.garlock@gmail.com>
2021-08-05 06:37:36 -0400

    first commit

--- ip_update.py
+++ ip_update.py
@@ -4,7 +4,6 @@ from passwords import return_username, return_password
 my_ip = requests.get('https://myip.dnsomatic.com/')
 my_ip = str(my_ip.text)
 
-
 url = f'https://{return_username(1)}:{return_password(1)}@domains.google.com/nic/update?hostname=bengarlock.com&myip={my_ip}'
 update_ip_response1 = requests.post(url)
 
